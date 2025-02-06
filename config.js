8f5eed2d07069a6870db5d32cf55f8f0bb203b44
赵轩境 <110761588+zhaoxuanjing@users.noreply.github.com>
2025-02-06 17:32:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const config = {
   ],
   qmsgKey: "6ffb29905bb6fb1221543945710c12d6", //qmsg酱key
   qmsgTo: "2794086070", //推送的qq号,用,分隔(可选)
-  wxPusherAppToken: "", // wxPusher 的 appToken
+  wxPusherAppToken: "AT_gnlAtKQD3Kno3KrwYOIosk0o1JQXgbI2", // wxPusher 的 appToken
   // TODO: feat: (wxpusher/topic) support wxpusher/topic push
 };
 
